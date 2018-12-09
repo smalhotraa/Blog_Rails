4594e6acf6e896f346645d3dfddf4d3446e82f7e
smalhotra <smalhotra2629@gmail.com>
2018-12-09 17:21:57 +0530

    Added secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ApplicationRecord
              length: {minimum: 3, maximum: 25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i            
-   validates :email, presence: true, length: { minimum: 3, maximum: 105 },
+  validates :email, presence: true, length: { minimum: 3, maximum: 105 },
              uniqueness: { case_sensitive: false },
              format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 
 end
